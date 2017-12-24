c8f2ca243e3d648395752775098eb060b30dc029
Michael Gutman <michaelgutman@hotmail.com>
2017-12-24 15:35:36 -0500

    Add filled fn to see if a puzzle is solved

--- solver.py
+++ solver.py
@@ -9,3 +9,9 @@ examplePuzz = [
             [0, 0, 2, 0, 0, 0, 0, 6, 0]
             [0, 0, 1, 0, 7, 0, 0, 0, 0]
             [4, 0, 0, 0, 0, 0, 0, 5, 0]]
+
+def filled(puzzle):
+    '''Determines if the puzzle is entirely filled.'''
+    for e in enumerate(puzzle):
+        if 0 in e: return False
+    return True
