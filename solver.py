8af5efde073dba5b6943ca658b67b57d71ae424f
Michael Gutman <michaelgutman@hotmail.com>
2017-12-24 16:14:42 -0500

    Add find firstUnused fn

--- solver.py
+++ solver.py
@@ -1,17 +1,24 @@
 # solver.py
 examplePuzz = [
-            [0, 1, 0, 2, 0, 0, 0, 3, 0]
-            [2, 0, 0, 0, 0, 6, 1, 0, 0]
-            [0, 0, 4, 0, 0, 0, 0, 2, 0]
-            [0, 2, 0, 0, 0, 0, 0, 0, 1]
-            [0, 0, 0, 0, 1, 0, 0, 5, 0]
-            [1, 8, 0, 9, 0, 2, 0, 0, 0]
-            [0, 0, 2, 0, 0, 0, 0, 6, 0]
-            [0, 0, 1, 0, 7, 0, 0, 0, 0]
+            [0, 1, 0, 2, 0, 0, 0, 3, 0],
+            [2, 0, 0, 0, 0, 6, 1, 0, 0],
+            [0, 0, 4, 0, 0, 0, 0, 2, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1, 0, 0, 5, 0],
+            [1, 8, 0, 9, 0, 2, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0, 6, 0],
+            [0, 0, 1, 0, 7, 0, 0, 0, 0],
             [4, 0, 0, 0, 0, 0, 0, 5, 0]]
 
 def filled(puzzle):
     '''Determines if the puzzle is entirely filled.'''
-    for e in enumerate(puzzle):
-        if 0 in e: return False
+    for row in puzzle:
+        if 0 in row: return False
     return True
+
+def firstUnused(puzzle):
+    '''Returns the [row, col] of the topmost-leftmost 0 in puzzle'''
+    for i, row in enumerate(puzzle):
+        zeroAt = e.index(0)
+        if zeroAt > -1:
+            return [i, zeroAt]
