ad52287dde4b370ea43f85e8db0e60405074eac7
Michael Gutman <michaelgutman@hotmail.com>
2017-12-30 12:08:08 -0500

    Accept input and create solution for solver

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QDialog
 from GUI.menu_ui import Ui_Menu
 from GUI.solver_ui import Ui_Solver
+import solver
 
 class Menu(QDialog, Ui_Menu):
     def __init__(self, parent=None):
@@ -17,6 +18,25 @@ class Solver(QDialog, Ui_Solver):
     def __init__(self, parent=None):
         super(Solver, self).__init__(parent)
         self.setupUi(self)
+        self.buttonBox.clicked.connect(self.solveClick)
+
+    def solveClick(self):
+        puzzle = []
+        for i in range(9):
+            row = []
+            for j in range(9):
+                item = self.tableWidget.item(i, j)
+                if item == None:
+                    item = '0'
+                else:
+                    item = item.text()
+                if item not in list('123456789'):
+                    item = '0'
+                row.append(int(item))
+            puzzle.append(row[:])
+        soln = solver.solve(puzzle)
+        for i in range(9):
+            print(soln[i])
 
 def main():
     app = QApplication(sys.argv)
